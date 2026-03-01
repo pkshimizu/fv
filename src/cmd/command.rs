72d4c16910d0e6932d96cf9168762a281348f73e
shimizu.kenji <shimizu.kenji@noncore.net>
2026-03-01 15:04:25 +0900

    refactor: FilerCursorをMoveCursorにリネーム
    - filer_cursor.rsをmove_cursor.rsにリネーム
    - FilerCursor*コマンドをMoveCursor*コマンドにリネーム
    - より汎用的な命名に変更

--- src/cmd/command.rs
+++ src/cmd/command.rs
@@ -1,11 +1,11 @@
-use crate::cmd::{change_dir, filer_cursor, quit};
+use crate::cmd::{change_dir, move_cursor, quit};
 use crate::state::AppState;
 
 pub enum Command {
-    FilerCursorUp,
-    FilerCursorDown,
-    FilerCursorLeft,
-    FilerCursorRight,
+    MoveCursorUp,
+    MoveCursorDown,
+    MoveCursorLeft,
+    MoveCursorRight,
     ChangeDir,
     Quit,
     None,
@@ -14,10 +14,10 @@ pub enum Command {
 impl Command {
     pub fn exec(self, state: &mut AppState) {
         match self {
-            Command::FilerCursorUp => filer_cursor::up(state),
-            Command::FilerCursorDown => filer_cursor::down(state),
-            Command::FilerCursorLeft => filer_cursor::first(state),
-            Command::FilerCursorRight => filer_cursor::last(state),
+            Command::MoveCursorUp => move_cursor::up(state),
+            Command::MoveCursorDown => move_cursor::down(state),
+            Command::MoveCursorLeft => move_cursor::first(state),
+            Command::MoveCursorRight => move_cursor::last(state),
             Command::ChangeDir => change_dir::exec(state),
             Command::Quit => quit::exec(state),
             Command::None => {}
