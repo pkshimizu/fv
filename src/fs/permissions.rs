257bee7cfecba9a882c0de73360c09dfa8951291
shimizu.kenji <shimizu.kenji@noncore.net>
2026-03-14 19:57:22 +0900

    chore: VPermissionsの未使用メソッドに警告抑制を追加
    - 将来使用予定の権限チェックメソッドにallow(dead_code)を追加

--- src/fs/permissions.rs
+++ src/fs/permissions.rs
@@ -11,38 +11,47 @@ impl VPermissions {
         Self { permissions }
     }
 
+    #[allow(dead_code)]
     pub fn owner_read(&self) -> bool {
         self.permissions.mode() & 0o400 != 0
     }
 
+    #[allow(dead_code)]
     pub fn owner_write(&self) -> bool {
         self.permissions.mode() & 0o200 != 0
     }
 
+    #[allow(dead_code)]
     pub fn owner_exec(&self) -> bool {
         self.permissions.mode() & 0o100 != 0
     }
 
+    #[allow(dead_code)]
     pub fn group_read(&self) -> bool {
         self.permissions.mode() & 0o040 != 0
     }
 
+    #[allow(dead_code)]
     pub fn group_write(&self) -> bool {
         self.permissions.mode() & 0o020 != 0
     }
 
+    #[allow(dead_code)]
     pub fn group_exec(&self) -> bool {
         self.permissions.mode() & 0o010 != 0
     }
 
+    #[allow(dead_code)]
     pub fn other_read(&self) -> bool {
         self.permissions.mode() & 0o004 != 0
     }
 
+    #[allow(dead_code)]
     pub fn other_write(&self) -> bool {
         self.permissions.mode() & 0o002 != 0
     }
 
+    #[allow(dead_code)]
     pub fn other_exec(&self) -> bool {
         self.permissions.mode() & 0o001 != 0
     }
