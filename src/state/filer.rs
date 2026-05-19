981672554605cffaba921a18a5c9c4bbd3b7f408
shimizu.kenji <shimizu.kenji@noncore.net>
2026-05-20 07:25:28 +0900

    style: エラー復元処理のフォーマットを整理

--- src/state/filer.rs
+++ src/state/filer.rs
@@ -368,13 +368,9 @@ impl FilerState {
                         self.dir_load_rx = None;
                         // エラー時は元のディレクトリに戻して同期リロード
                         if let Some(prev_dir) = self.prev_dir.take() {
-                            if let Err(restore_err) =
-                                self.load_current_dir_sync(Some(prev_dir))
-                            {
+                            if let Err(restore_err) = self.load_current_dir_sync(Some(prev_dir)) {
                                 if let Some(err) = &mut self.load_error {
-                                    err.push_str(&format!(
-                                        " (restore failed: {restore_err})"
-                                    ));
+                                    err.push_str(&format!(" (restore failed: {restore_err})"));
                                 }
                             } else {
                                 self.file_table_state.select(Some(0));
