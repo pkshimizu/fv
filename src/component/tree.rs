18ca4853c83896178d1da9597ce8d9720d75d99e
shimizu.kenji <shimizu.kenji@noncore.net>
2026-05-21 10:04:12 +0900

    fix: ツリービューの選択カーソルを > に戻す

--- src/component/tree.rs
+++ src/component/tree.rs
@@ -78,7 +78,7 @@ impl Component for TreeComponent {
 
         let table = Table::new(rows, [Constraint::Fill(1)])
             .block(block)
-            .highlight_symbol("+ ")
+            .highlight_symbol("> ")
             .row_highlight_style(Style::default().add_modifier(Modifier::UNDERLINED));
         frame.render_stateful_widget(table, area, &mut self.state.table_state);
     }
